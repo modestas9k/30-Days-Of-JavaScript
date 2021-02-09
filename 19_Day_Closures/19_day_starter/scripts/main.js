367ea97ccbf767fda92ac528868030544cae6815
Modestas Skirius <modestas.skirius@gmail.com>
2021-02-09 09:40:28 +0200

    day 19 task complete

--- 19_Day_Closures/19_day_starter/scripts/main.js
+++ 19_Day_Closures/19_day_starter/scripts/main.js
@@ -1,2 +1,41 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+//
+//// Exercise 1
+//
+
+// 1. Create a closure which has one inner function.
+//
+function alertName() {
+  const name = "Modestas";
+  function displayName() {
+    alert(name);
+  }
+  displayName();
+}
+// alertName();
+
+// 2. Create a closure which has three inner functions.
+//
+const counter = () => {
+  let privateCounter = 0;
+  function changeBy(num) {
+    privateCounter += num;
+  }
+  return {
+    increment: () => {
+      changeBy(1);
+    },
+    decrement: () => {
+      changeBy(-1);
+    },
+    value: () => privateCounter,
+  };
+};
+
+const counter1 = counter();
+const counter2 = counter();
+
+counter1.increment();
+counter1.increment();
+counter2.increment();
+console.log(counter1.value()); // 2
+console.log(counter2.value()); // 1
