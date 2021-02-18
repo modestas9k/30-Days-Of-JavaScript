7b33df180e09b1c6e726e259e091ed2a048e870c
Modestas Skirius <modestas.skirius@gmail.com>
2021-02-18 17:41:20 +0200

    day 29 task complete

--- 29_Day_Mini_project_animating_characters/29_day_starters/project_1/scripts/main.js
+++ 29_Day_Mini_project_animating_characters/29_day_starters/project_1/scripts/main.js
@@ -1,2 +1,36 @@
-console.log(countries)
-alert('Open the console and check if the countries has been loaded')
+const fonts = [
+  "Lato",
+  "Montserrat",
+  "Nunito",
+  "Oswald",
+  "Raleway",
+  "Roboto",
+  "Raleway Dots",
+  "Aldrich",
+];
+const fontWeight = [300, 400, 500, 600, 700, 800, 900];
+
+function randomNumber(arr) {
+  return Math.floor(Math.random() * arr.length);
+}
+function randomColor() {
+  return "#" + Math.floor(Math.random() * 16777215).toString(16);
+}
+const sentence = "30 DAYS OF JAVASCRIPT CHALLENGE 2021";
+const letters = sentence.split("");
+const body = document.querySelector("body");
+
+setInterval(() => {
+  body.innerHTML = "";
+  body.style.backgroundColor = randomColor();
+  body.style.fontFamily = fonts[randomNumber(fonts)];
+  body.style.fontWeight = fontWeight[randomNumber(fontWeight)];
+
+  letters.map((letter) => {
+    let span = document.createElement("span");
+    span.innerHTML = letter;
+    span.style.color = randomColor();
+    span.className = "span";
+    body.append(span);
+  });
+}, 2000);
